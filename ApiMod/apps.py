3162e4cb108770172c8716788ff8c7efeb583142
ws <190510995@qq.com>
2019-05-09 22:05:47 +0800

    init

---
+++ ApiMod/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ApimodConfig(AppConfig):
+    name = 'ApiMod'
---
+++ MainViewsMod/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MainviewsmodConfig(AppConfig):
+    name = 'MainViewsMod'
