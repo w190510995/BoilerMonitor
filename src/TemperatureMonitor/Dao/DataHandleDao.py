b5ee0874a8aed7b7c3795c77a964b501471e7b2a
ws <190510995@qq.com>
2019-05-10 23:52:58 +0800

    init6

--- src/TemperatureMonitor/Dao/DataHandleDao.py
+++ src/TemperatureMonitor/Dao/DataHandleDao.py
@@ -85,7 +85,69 @@ def DataHandle(tagName,tagValue,tagDesc,thresholdValue,area):
     :param thresholdValue:  报警定值
     :param area: 处理数据所属的区域
     """
-    pass
+    get_tagname_catch = cache.get(tagName) #根据标签名获取redis中对应的数据
+
+    if get_tagname_catch is None: # redis中无标签对应的数据，则新建一条数据缓存到redis
+
+
+        if tagValue >= thresholdValue: #标签当前值大于定值
+
+               # 超过定值，放入redis
+                cache.set(tagName, {
+                    'tagDesc':tagDesc,#标签描述
+                    'curentValue': tagValue, #当前值
+                    'maxValue': tagValue, #最大值，第一次最大值为当前值
+                    'thresholdValuet': thresholdValue,
+                    'beginDate': datetime.now(),
+                    'area':area,
+                    'endDate': '',
+                    'status': 1  # 1：报警  0:报警结束
+                },
+                 timeout=None,  # 永不超时
+                )
+
+
+
+    else:
+
+        if get_tagname_catch['status'] == 1: #  1：报警未结束
+            if tagValue >= thresholdValue: #大于定值 更新缓存maxvalue数据
+                maxValue = get_tagname_catch['maxValue']
+
+                if tagValue > maxValue:
+                    get_tagname_catch['maxValue'] = tagValue  # 更新状态
+
+                get_tagname_catch['curentValue'] = tagValue  # 更新状态
+                cache.set(tagName, get_tagname_catch, timeout=None)  # 更新数据到redis
+
+            else:
+                get_tagname_catch['status'] = 0  # 更新状态
+                get_tagname_catch['endDate'] = datetime.now()  # 结束日期
+                cache.set(tagName, get_tagname_catch, timeout=None)  # 更新数据到redis
+
+
+
+        elif get_tagname_catch['status'] == 0:  #该条报警状态已结束，保存到数据库，随后删除redis中数据
+
+            beginTime1 = get_tagname_catch['beginDate'],
+            endTime1 = get_tagname_catch['endDate'],
+
+            if BoilerUrl.OPREATION_CLASSIC  == 0:
+                BoilerUrl.OPREATION_CLASSIC = UpdataPeriodTeam()  # 更新OPREATION_CLASSIC
+
+            persistence = TempratureAlermValue(
+                name=tagName,
+                desc=get_tagname_catch['tagDesc'],
+                area=get_tagname_catch['area'],
+                classic= boilerUrls.OPREATION_CLASSIC,
+                maxValue= get_tagname_catch['maxValue'],
+                thresholdValuet= get_tagname_catch['thresholdValuet'],
+                beginTime= get_tagname_catch['beginDate'],
+                endTime= get_tagname_catch['endDate'],
+                tiemDiff= (endTime1[0]-beginTime1[0]).seconds)
+
+            persistence.save() #保存到数据库
+            cache.delete(tagName)  # 报警结束 删除缓存
 
 
 
